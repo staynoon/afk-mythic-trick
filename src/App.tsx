b74da9fbbe6fbf01a0fcc205ec8bc80127a190cf
Zhuo Fan Bao <zhuofanb@gmail.com>
2021-02-13 15:12:42 -0500

    font import

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+type Props = {};
+
+type State = {
+  timeString: string;
+};
+
+class App extends React.Component<Props,State>{
+  render(){
+    return (
+      <div className="App">
+        <h1>AFK Mythic Trick Timer</h1>
+        <h2>Emblems Timer</h2>
+        <p>10:30</p>
+        <h2>Mythic Gear Timer</h2>
+        <p>10:30</p>
+        <h2>Stone Timer</h2>
+        <p>10:30</p>
+      </div>
+  )
+  }
+};
 
 export default App;
