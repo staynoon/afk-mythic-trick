534df46fc8b85a9377a70541f8ee6da59b257d3d
Zhuo Fan Bao <zhuofanb@gmail.com>
2021-02-13 23:20:27 -0500

    layout update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,52 @@
 import * as React from "react";
+import $ from "jquery";
 
 import './App.css';
 
-type Props = {};
+interface Props {};
 
-type State = {
+interface State {
   timeString: string;
 };
 
 class App extends React.Component<Props,State>{
+  constructor(props: Props){
+    super(props);
+    this.state = {
+      timeString: "10:30"
+    }
+  }
+
   render(){
     return (
       <div className="App">
-        <h1>AFK Mythic Trick Timer</h1>
-        <h2>Emblems Timer</h2>
-        <p>10:30</p>
-        <h2>Mythic Gear Timer</h2>
-        <p>10:30</p>
-        <h2>Stone Timer</h2>
-        <p>10:30</p>
+        <div className="title-box">
+          <h1 className="stroke-single">AFK Mythic Trick Timer</h1>
+        </div>
+        <div className="column-container">
+          <div className="timer-box">
+            <h2 className="stroke-single">Emblems Timer</h2>
+            <h3 className="stroke-single">10:30</h3>
+          </div>
+          <div className="timer-box">
+            <h2 className="stroke-single">Mythic Gear Timer</h2>
+            <h3 className="stroke-single">10:30</h3>
+          </div>
+          <div className="timer-box">
+            <h2 className="stroke-single">Stone Timer</h2>
+            <h3 className="stroke-single">10:30</h3>
+          </div>
+        </div>
+        <p className="stroke-single">Come back on January 13, 2021 at 6:08 PM for a 51% chance to get a stone!</p>
       </div>
-  )
+    )
   }
 };
+
+$(function(){
+  $('.stroke-single').attr('title', function(){
+    return $(this).html();
+  });
+});
 
 export default App;
