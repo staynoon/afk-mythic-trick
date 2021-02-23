a60c007963a258505be0e4c39debbbf042ff8fef
Zhuo Fan Bao <zhuofanb@gmail.com>
2021-02-23 14:20:03 -0500

    timer query

--- afk-mythic-trick-react/src/userInput/Trickbutton.tsx
+++ afk-mythic-trick-react/src/userInput/Trickbutton.tsx
@@ -29,11 +29,29 @@ interface Props {
   setRate: React.Dispatch<React.SetStateAction<string>>
 }
 
+interface AFKTimers {
+  Stone: string,
+  Emblems: string,
+  Gear: string
+}
+
 let emblemTime: number;
 let gearTime: number;
 let stoneTime: number;
 
+let remainingEmblemTime: number;
+let remainingGearTime: number;
+let remainingStoneTime: number;
+
 const monthNames: Array<string> = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+async function getTimer (chapter: number, stage: number): Promise<AFKTimers> {
+  let requestURL: string = '/progression/' + chapter + '/' + stage;
+  const responseData = await fetch(requestURL)
+    .then(response => response.json());
+  
+  return responseData;
+}
 
 function Trickbutton(props: Props) {
   const [slide, setSlide] = React.useState<string>(props.rate);
@@ -45,41 +63,36 @@ function Trickbutton(props: Props) {
   function buttonClick(): void {
     let mythicClearTime = `${monthNames[props.time.month]} ${props.time.day}, ${props.time.year} ${props.time.hour}:${props.time.minute}`;
     localStorage.setItem("mythicClearTime",mythicClearTime);
-
-    let requestURL = '/progression/' + props.progression.chapter + '/' + props.progression.stage;
-    fetch(requestURL)
-      .then(response => response.json())
-      .then(data => {
-        stoneTime = data.Stone;
-        emblemTime = data.Emblem;
-        gearTime = data.Gear
-      })
-
     let claimTime = new Date(mythicClearTime);
     let chance: number = parseInt(slide,10)/100;
-    let remainingStoneTime = Math.round(claimTime.getTime()/1000 + stoneTime * chance - Date.now()/1000);
-    let remainingGearTime = Math.round(claimTime.getTime()/1000 + gearTime * chance - Date.now()/1000);
-    let remainingEmblemTime = Math.round(claimTime.getTime()/1000 + emblemTime * chance - Date.now()/1000);
-    if (remainingEmblemTime <= 0) {
-      props.setTimer.setEmblem(0);
-    } else {
-      props.setTimer.setEmblem(remainingEmblemTime);
-    }
-    if (remainingGearTime <= 0) {
-      props.setTimer.setGear(0);
-    } else {
-      props.setTimer.setGear(remainingGearTime);
-    }
-    if (remainingStoneTime <= 0) {
-      props.setTimer.setStone(0);
-    } else {
-      props.setTimer.setStone(remainingStoneTime);
-    }
-
-    props.setRate(slide);
 
     localStorage.setItem("stageProgression",`${props.progression.chapter}-${props.progression.stage}`);
     localStorage.setItem("dropChance",slide);
+
+    getTimer(props.progression.chapter,props.progression.stage)
+      .then(data => {
+        remainingStoneTime = Math.round(claimTime.getTime()/1000 + parseInt(data.Stone,10) * chance - Date.now()/1000);
+        remainingGearTime = Math.round(claimTime.getTime()/1000 + parseInt(data.Gear,10) * chance - Date.now()/1000);
+        remainingEmblemTime = Math.round(claimTime.getTime()/1000 + parseInt(data.Emblems,10) * chance - Date.now()/1000);
+
+        if (remainingEmblemTime <= 0) {
+          props.setTimer.setEmblem(0);
+        } else {
+          props.setTimer.setEmblem(remainingEmblemTime);
+        }
+        if (remainingGearTime <= 0) {
+          props.setTimer.setGear(0);
+        } else {
+          props.setTimer.setGear(remainingGearTime);
+        }
+        if (remainingStoneTime <= 0) {
+          props.setTimer.setStone(0);
+        } else {
+          props.setTimer.setStone(remainingStoneTime);
+        }
+
+        props.setRate(slide);
+      })
   }
 
   return (
