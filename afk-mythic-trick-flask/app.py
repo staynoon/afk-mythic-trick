ea1728066701aaeb264a3896ae5dddd05a55ae9b
Zhuo Fan Bao <zhuofanb@gmail.com>
2021-02-24 08:16:18 -0500

    cors implementation

--- afk-mythic-trick-flask/app.py
+++ afk-mythic-trick-flask/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+cors = CORS(app)
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
